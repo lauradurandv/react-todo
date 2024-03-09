66f23e328914741e2af6a27ef1a877e32e00ab01
lauradurandv <26388264+lauradurandv@users.noreply.github.com>
2024-03-09 10:43:40 -0500

    introducing state and props

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,9 +1,19 @@
-function AddTodoForm(){
+function AddTodoForm(props){
+
+    function handleAddTodo(event){
+        event.preventDefault();
+        const todoTitle = event.target.title.value;
+        console.log(todoTitle);
+        //Pass to app.js
+        props.onAddTodo(todoTitle);
+
+    }
+
     return(
-        <form>
+        <form onSubmit={handleAddTodo} >
             <label htmlFor="todoTitle">Title</label>
-            <input id="todoTitle"/>
-            <button>Add</button>
+            <input name="title" id="todoTitle"/>
+            <button type="submit">Add</button>
         </form>
     )
 }
---
+++ src/TodoListItem.jsx
@@ -0,0 +1,6 @@
+function TodoListItem(props){
+    return(
+        <li>{props.todo.title}</li>
+    );
+}
+export default TodoListItem;
